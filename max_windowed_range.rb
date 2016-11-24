e6576b00f87d50160d14657fa63b6f7995189e80
Adom Hartell <adomhartell@gmail.com>
2016-11-24 15:44:11 -0800

    Solve window range problem with MinMaxStackQueue

--- max_windowed_range.rb
+++ max_windowed_range.rb
@@ -1,3 +1,5 @@
+require_relative 'minmaxstackqueue'
+
 def max_windowed_range(array, window_size)
   max_range = nil
   (0..(array.size - window_size)).each do |start_idx|
@@ -9,5 +11,18 @@ def max_windowed_range(array, window_size)
     end
   end
 
+  max_range
+end
+
+def fancy_windowed_range(array, window_size)
+  window = MinMaxStackQueue.new
+  window_size.times { |i| window.enqueue(array[i]) }
+  max_range = window.max - window.min
+  (window_size...array.length).each do |index|
+    window.dequeue
+    window.enqueue(array[index])
+    new_range = window.max - window.min
+    max_range = new_range if new_range > max_range
+  end
   max_range
 end
