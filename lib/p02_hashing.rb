f742cbfe8676934847a225c35f55370ec7549fd9
Adom Hartell <adomhartell@gmail.com>
2016-11-25 11:35:02 -0800

    Make hashing functions for string, hash, array

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,36 @@ end
 
 class Array
   def hash
+    fixnum = to_fixnum
+    fixnum.hash
   end
+
+  private
+
+  def to_fixnum
+    size = self.size
+    self.each_with_index do |el, index|
+      size = size + el * index
+    end
+
+    size
+  end
+
 end
 
 class String
   def hash
+    fixnum = to_fixnum
+    fixnum.hash
+  end
+
+  def to_fixnum
+    sum = 0
+    self.chars.each_with_index do |char, index|
+      sum += char.ord * index
+    end
+
+    sum
   end
 end
 
@@ -16,6 +41,28 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    fixnum = to_fixnum
+    fixnum.hash
   end
+
+  def to_fixnum
+    arr = []
+    self.each do |k, v|
+      arr << [k, v]
+    end
+
+    arr.sort!
+    hash = 0
+    arr.each do |k, v|
+      k_fix = k.is_a?(Fixnum) ? k : k.inspect.to_fixnum
+      v_fix = v.is_a?(Fixnum) ? v : v.inspect.to_fixnum
+
+      hash = hash + k_fix + v_fix
+    end
+    hash
+
+  end
+
+
+
 end
