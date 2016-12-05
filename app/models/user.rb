a7746b78182a16825f0f73b46be793b229748849
Adom Hartell <adomhartell@gmail.com>
2016-12-05 15:13:12 -0800

    add ContactShare and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
 
+  has_many :contacts, dependent: :destroy
+  has_many :contact_shares, dependent: :destroy
+  has_many :shared_contacts, through: :contact_shares, source: :contact
+
   validates :username, presence: true, uniqueness: true
+
 end
