3b3656d8418f4cfa2bf3ab3378205dc1ca288e56
Adom Hartell <adomhartell@gmail.com>
2016-12-05 13:54:48 -0800

    add #create_user to users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,10 @@ class UsersController < ApplicationController
   def index
     render json: User.all
   end
+
+  def create
+    user = User.new(params[:user].permit(:name, :email))
+    user.save!
+    render json: user
+  end
 end
--- bin/my_script.rb
+++ bin/my_script.rb
@@ -14,3 +14,19 @@ def index_users
 end
 
 index_users
+
+def create_user(name, email)
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users.json'
+  ).to_s
+
+  puts RestClient.post(
+    url,
+    { user: { name: name, email: email } }
+  )
+end
+
+create_user("Gizmo", "gizmo@gizmo.gizmo")
