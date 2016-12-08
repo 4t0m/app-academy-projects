9cba052ccca68a9f9371b7c08dfe4882378095b0
Adom Hartell <adomhartell@gmail.com>
2016-12-08 10:25:33 -0800

    add Albums migration and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 	resources :users, only: [:create, :new, :show]
   resource :sessions, only: [:create, :new, :destroy]
 	resources :bands
+	resources :albums, except: :index
 end
