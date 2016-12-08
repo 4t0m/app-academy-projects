e95c559c331d30d1c833db86b0eec5f5a853a230
Adom Hartell <adomhartell@gmail.com>
2016-12-08 11:12:25 -0800

    update route for albums#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-	
+
 	resources :users, only: [:create, :new, :show]
   resource :sessions, only: [:create, :new, :destroy]
-	resources :bands
-	resources :albums, except: :index
+	resources :bands do
+		resources :albums, only: :new
+	end
+	resources :albums, except: [:index, :new]
 end
