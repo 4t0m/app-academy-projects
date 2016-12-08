6e7e936bf2229ab45cd978172913e1ec506f8c6e
Adom Hartell <adomhartell@gmail.com>
2016-12-08 09:36:57 -0800

    add UsersController and User routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+	def new
+	  @user = User.new
+		render :new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			session[:session_token] = @user.session_token
+		  redirect_to user_url(@user)	
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def user_params
+		params
+			.require(:user)
+			.permit(:email, :password_digest, :session_token)
+	end
+
+end
