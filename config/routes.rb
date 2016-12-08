94dd2edbc6289b5f41e643696dea1f04e5a870bf
Adom Hartell <adomhartell@gmail.com>
2016-12-08 15:46:35 -0800

    create render and delete notes

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,16 @@ Rails.application.routes.draw do
 	resources :bands do
 		resources :albums, only: :new
 	end
-	
+
 	resources :albums, except: [:index, :new] do
 		resources :tracks, only: :new
 	end
 
-	resources :tracks, except: [:index, :new]
+	resources :tracks, except: [:index, :new] do
+		resources :notes, only: :new
+	end
+
+	resources :notes, only: [:create, :destroy]
+
+
 end
