a1857d3f6ddd5d9cf92e9acd6b0ab84ffff46bea
Adom Hartell <adomhartell@gmail.com>
2016-12-08 11:36:38 -0800

    add titles column to tracks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208191545) do
+ActiveRecord::Schema.define(version: 20161208193341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20161208191545) do
     t.text     "lyrics"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title",      null: false
   end
 
   add_index "tracks", ["album_id"], name: "index_tracks_on_album_id", using: :btree
