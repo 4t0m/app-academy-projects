75c45ad0dcfdab6d419fb95962582809a045205f
Adom Hartell <adomhartell@gmail.com>
2016-12-09 10:31:05 -0800

    add User migration and model

---
+++ RedditClone/app/models/user.rb
@@ -0,0 +1,48 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def is_password?(password)
+    bcryt_obj = BCrypt::Password.new(self.password_digest)
+    bcryt_obj.is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(128)
+  end
+end
