0a2f20427603833d46499ecc906ea90bfaa95b28
Adom Hartell <adomhartell@gmail.com>
2016-12-12 10:29:59 -0800

    set up rspec and capybara

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,16 @@ module GoalApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+		config.generators do |g|
+			g.test_frameword :rspec,
+				fixtures: false,
+				view_specs: false,
+				helper_specs: false,
+				routing_specs: false,
+				controller_specs: true,
+				request_specs: false
+		end
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
