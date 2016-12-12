104eaa0ac1facb63764ecb4ebab1bbf740c1d72c
Adom Hartell <adomhartell@gmail.com>
2016-12-12 10:42:19 -0800

    add model tests for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) do
+    FactoryGirl.build(:user,
+      username: "adom",
+      password: "my_password")
+  end
+
+  it { should validate_presence_of(:username) }
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_length_of(:password).is_at_least
+
+
+  describe "password encryption" do
+  it "does not save passwords to the database" do
+    User.create!(username: "4t0m", password: "123456")
+    user = User.find_by_username("4t0m")
+    expect(user.password).not_to be("123456")
+  end
+
+  it "encrypts the password using BCrypt" do
+    expect(BCrypt::Password).to receive(:create)
+    User.new(username: "4t0m", password: "123456")
+  end
+
 end
