4a3349e1956200955c91bdf98fff882f21040c35
Adom Hartell <adomhartell@gmail.com>
2016-12-12 11:42:49 -0800

    add feature specs for authentication

---
+++ spec/features/auth_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+require 'spec_helper'
+
+feature "the signup process" do
+  scenario "has a new user page" do
+    visit new_user_url
+    expect(page).to have_content "Sign Up!"
+  end
+
+  scenario "shows username on the homepage after signup" do
+    sign_up_as('hello_world')
+    expect(page).to have_content "Welcome hello_world"
+  end
+end
+
+feature "logging in" do
+  let!(:hello_world) { FactoryGirl.create(:user_hw) }
+
+  scenario "shows username on the homepage after login" do
+    login_as(hello_world)
+    expect(page).to have_content "Welcome hello_world"
+  end
+end
+
+feature "logging out" do
+  let!(:hello_world) { FactoryGirl.create(:user_hw) }
+
+  scenario "begins with logged out state" do
+    visit root_url
+    expect(page).to have_content "Log In"
+  end
+
+  scenario "doesn't show username on the homepage after logout" do
+    login_as(hello_world)
+    click_button "Log Out"
+    expect(page).not_to have_content "hello_world"
+  end
+end
