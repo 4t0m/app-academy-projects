d2af8574bd76d8b86f2e8a3c10cc731f39caedde
Adom Hartell <adomhartell@gmail.com>
2016-12-12 12:04:02 -0800

    update rails_helper to include support files in specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,11 +24,17 @@ require 'rspec/rails'
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include AuthFeaturesHelper, type: :feature
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
