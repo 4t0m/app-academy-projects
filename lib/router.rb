b4a43927e4b7cf518e38f2c466e96d5d44df0481
Adom Hartell <adomhartell@gmail.com>
2016-12-13 15:59:19 -0800

    make route_params argument optional in ControllerBase#initialize

--- lib/router.rb
+++ lib/router.rb
@@ -42,7 +42,7 @@ class Router
   # evaluate the proc in the context of the instance
   # for syntactic sugar :)
   def draw(&proc)
-    instance_eval(&proc)
+    self.instance_eval(&proc)
   end
 
   # make each of these methods that
