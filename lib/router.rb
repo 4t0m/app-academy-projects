64578225142e37b025ac1a1638f83576f9fff922
Adom Hartell <adomhartell@gmail.com>
2016-12-13 15:44:19 -0800

    Router creates routes for all HTTP methods

--- lib/router.rb
+++ lib/router.rb
@@ -26,30 +26,43 @@ class Route
 end
 
 class Router
+
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    route_parameters = [pattern, method, controller_class, action_name]
+    @routes << Route.new(*route_parameters)
   end
 
   # evaluate the proc in the context of the instance
   # for syntactic sugar :)
   def draw(&proc)
+    instance_eval(&proc)
   end
 
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
   end
 
   # should return the route that matches this request
   def match(req)
+    routes.find { |route| route.matches?(req) }
   end
 
   # either throw 404 or call run on a matched route
   def run(req, res)
+    matching_route = match(req)
+    return res.status = 404 if matching_route.nil?
+
+    matching_route.run(req, res)
   end
 end
