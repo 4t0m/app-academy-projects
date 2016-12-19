78802d748df563062b1017989f5919097a55ee5b
Adom Hartell <adomhartell@gmail.com>
2016-12-19 14:08:27 -0800

    add main js file

---
+++ skeleton/js/main.js
@@ -0,0 +1,8 @@
+const HanoiGame = require('../../solution/game.js');
+const HanoiView = require('./hanoiview.js');
+
+$( () => {
+  let $rootEl = $('.hanoi');
+  let game = new HanoiGame();
+  let hanoiView = new HanoiView(game, $rootEl);
+});
