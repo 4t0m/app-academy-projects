c4c01f427f906455563ec27bd4dfde1615a3d9c3
Adom Hartell <adomhartell@gmail.com>
2016-12-19 10:54:35 -0800

    add #setupBoard

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require('./ttt-view.js');// require appropriate file
+const Game = require('./game.js');// require appropriate file
+
 
 $( () => {
-  // Your code here
+  let game = new Game();
+  let el = $(".ttt")
+  let view = new View(game, el);
+
+  
+
 });
--- js/ttt-view.js
+++ js/ttt-view.js
@@ -1,11 +1,21 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
+  }
 
   bindEvents() {}
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard() {
+    let grid = $("<ul></ul>");
+    for(let i = 0; i < 9; i++){
+      const li = $("<li></li>");
+      grid.appendChild(li);
+    }
+
+  }
 }
 
 module.exports = View;
