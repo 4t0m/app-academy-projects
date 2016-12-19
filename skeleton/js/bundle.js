ab4690f5f46253c905a65809b10039dc0fd0a16d
Adom Hartell <adomhartell@gmail.com>
2016-12-19 14:34:02 -0800

    attempt tower render logic

--- skeleton/js/bundle.js
+++ skeleton/js/bundle.js
@@ -142,12 +142,17 @@
 
 	  setupTowers(){
 	    let $pile1 = $("<ul>");
+	    $pile1.attr("id", `pile1`);
+	    $pile2.attr("id", `pile2`);
+	    $pile3.attr("id", `pile3`);
+
 	    let $pile2 = $("<ul>");
 	    let $pile3 = $("<ul>");
 	    for(let i = 3; i <= 1; i--){
-	      let $disc = $("<li>");
-	      $disc.attr("id", i);
-	      $pile1.append($disc);
+	      let $disk = $("<li>");
+	      $disk.attr("id", `disk${i}`);
+
+	      $pile1.append($disk);
 	    }
 	    this.$el.append($pile1);
 	    this.$el.append($pile2);
@@ -155,8 +160,17 @@
 	  }
 
 	  render(){
-	    this.game.towers.forEach(function (tower) {
-	      
+	    this.game.towers.forEach(function (tower, towerIdx) {
+	      for (let diskIdx = 1; diskIdx < 3; diskIdx++) {
+	        let $disk = $(`#disk${diskIdx}`);
+	        let $pile = $(`#pile${towerIdx}`);
+	        if (tower.includes(diskIdx) && $(`ul#pile${towerIdx} li#disk${diskIdx}`).length === 0) {
+	          $pile.append($disk);
+	        }
+	        if (!tower.includes(diskIdx) && $(`ul#pile${towerIdx} li#disk${diskIdx}`).length === 1) {
+	          $pile.remove($disk);
+	        }
+	      }
 	    });
 	  }
 	}
--- skeleton/js/hanoiview.js
+++ skeleton/js/hanoiview.js
@@ -8,12 +8,17 @@ class HanoiView {
 
   setupTowers(){
     let $pile1 = $("<ul>");
+    $pile1.attr("id", `pile1`);
+    $pile2.attr("id", `pile2`);
+    $pile3.attr("id", `pile3`);
+
     let $pile2 = $("<ul>");
     let $pile3 = $("<ul>");
     for(let i = 3; i <= 1; i--){
-      let $disc = $("<li>");
-      $disc.attr("id", i);
-      $pile1.append($disc);
+      let $disk = $("<li>");
+      $disk.attr("id", `disk${i}`);
+
+      $pile1.append($disk);
     }
     this.$el.append($pile1);
     this.$el.append($pile2);
@@ -21,8 +26,17 @@ class HanoiView {
   }
 
   render(){
-    this.game.towers.forEach(function (tower) {
-      
+    this.game.towers.forEach(function (tower, towerIdx) {
+      for (let diskIdx = 1; diskIdx < 3; diskIdx++) {
+        let $disk = $(`#disk${diskIdx}`);
+        let $pile = $(`#pile${towerIdx}`);
+        if (tower.includes(diskIdx) && $(`ul#pile${towerIdx} li#disk${diskIdx}`).length === 0) {
+          $pile.append($disk);
+        }
+        if (!tower.includes(diskIdx) && $(`ul#pile${towerIdx} li#disk${diskIdx}`).length === 1) {
+          $pile.remove($disk);
+        }
+      }
     });
   }
 }
