9a45923df47489f8071a3a8003ac98ccc32bf721
Adom Hartell <adomhartell@gmail.com>
2016-12-21 10:40:27 -0800

    FollowToggle class toggles button text and backend state

--- frontend/follow_toggle.js
+++ frontend/follow_toggle.js
@@ -3,6 +3,33 @@ class FollowToggle {
     this.$el = $(el);
     this.userId = this.$el.data("userId");
     this.followState = this.$el.data("initialFollowState");
+    this.$el.text(this.render());
+    this.$el.on("click", this.handleClick.bind(this));
+  }
+
+  render() {
+    if (this.followState === "Unfollowed") {
+      return "Follow!";
+    } else {
+      return "Unfollow!";
+    }
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    let type = this.followState === "Unfollowed" ? "POST" : "DELETE";
+    let nf = this.followState === "Followed" ? "Unfollowed" : "Followed";
+    let success = () => {
+      this.followState = nf;
+      this.$el.text(this.render());
+    }
+
+    $.ajax({
+      type: type,
+      dataType: "JSON",
+      url: `/users/${this.userId}/follow`,
+      success: success
+    });
   }
 }
 
