65fb57d69fe95ada78d97353471ec004519e3b5c
Adom Hartell <adomhartell@gmail.com>
2016-12-21 19:18:36 -0800

    add readme

---
+++ README.m
@@ -0,0 +1,9 @@
+# AJAX Twitter
+
+Implementation of the following Twitter features using AJAX.
++ Follow and Unfollow Users
+	+Toggles "follow" button without reloading the page.
++ Create Tweet 
+	+ Adds tweet to feed without reloading the page.
+	+ Updates remaining characters during user input.
+	+ Includes button to add variable numbers of mentioned users.
