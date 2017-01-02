45f75ef215aa675a5f707cfeda2b2526b6b3caae
Adom Hartell <adomhartell@gmail.com>
2017-01-02 14:52:57 -0800

    Add todosReducer to handle receiveTodo and receiveTodos

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,4 +1,5 @@
 import { RECEIVE_TODOS, RECEIVE_TODO } from "../actions/todo_actions";
+import merge from 'lodash/merge';
 
 const initialState = {
   1: {
@@ -15,17 +16,20 @@ const initialState = {
   },
 };
 
-const todosReducer = ( state = initialState, action) => {
+const todosReducer = (state = initialState, action) => {
   Object.freeze(state);
   switch (action.type) {
-    // case RECEIVE_TODOS:
-    //   return {
-    //
-    //   };
-    // case RECEIVE_TODO:
-    //   return {
-    //
-    //   };
+    case RECEIVE_TODOS:
+      const newState = {};
+      Object.keys(action.todos).forEach( key => (
+        newState[key] = action.todos[key]
+      ));
+      return newState;
+
+    case RECEIVE_TODO:
+      let updatedState = merge({}, state);
+      updatedState[action.todo.id] = action.todo;
+      return updatedState;
     default:
       return state;
   }
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import { allTodos } from './reducers/selectors';
 
+import { receiveTodos } from './actions/todo_actions';
+
 document.addEventListener("DOMContentLoaded", function(){
-  window.store = allTodos(configureStore.getState());  // REMOVE LATER
+  window.store = configureStore();  // REMOVE LATER
+  window.receiveTodos = receiveTodos; // REMOVE LATER
   ReactDOM.render(<h1>Todos App</h1>, document.getElementById('content'));
 });
