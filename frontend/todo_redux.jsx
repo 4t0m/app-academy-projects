0fd235003b99c25ef9b94da59bc68cb8ea82ff5b
Adom Hartell <adomhartell@gmail.com>
2017-01-02 12:12:21 -0800

    Add selectors

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import { allTodos } from './reducers/selectors';
 
 document.addEventListener("DOMContentLoaded", function(){
-  window.store = configureStore;  // REMOVE LATER
+  window.store = allTodos(configureStore.getState());  // REMOVE LATER
   ReactDOM.render(<h1>Todos App</h1>, document.getElementById('content'));
 });
