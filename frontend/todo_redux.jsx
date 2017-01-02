ba3fa6cc7750edc89388ed7eaab9c162dd3d3636
Adom Hartell <adomhartell@gmail.com>
2017-01-02 15:05:43 -0800

    Refactor entry file, create root.jsx and app.jsx in Components directory

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import { allTodos } from './reducers/selectors';
+import Root from './components/root';
 
 import { receiveTodos } from './actions/todo_actions';
 
 document.addEventListener("DOMContentLoaded", function(){
-  window.store = configureStore();  // REMOVE LATER
-  window.receiveTodos = receiveTodos; // REMOVE LATER
-  ReactDOM.render(<h1>Todos App</h1>, document.getElementById('content'));
+  // window.store = configureStore();  // REMOVE LATER
+  // window.receiveTodos = receiveTodos; // REMOVE LATER
+  ReactDOM.render(<Root store={configureStore()}/>, document.getElementById('content'));
 });
