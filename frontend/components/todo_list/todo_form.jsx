166e68c46364973c9c7747d8dc108af5e287d36e
Adom Hartell <adomhartell@gmail.com>
2017-01-02 17:59:40 -0800

    Add field to enter todo body

--- frontend/components/todo_list/todo_form.jsx
+++ frontend/components/todo_list/todo_form.jsx
@@ -4,28 +4,36 @@ import uniqueId from '../../util/util';
 class TodoForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { todo: "" };
+    this.state = { title: "", body: "" };
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.updateTodo = this.updateTodo.bind(this);
+    // this.updateTitle = this.updateTitle.bind(this);
+    // this.updateBody = this.updateBody.bind(this);
   }
 
   handleSubmit(e) {
     e.preventDefault();
-    let newTodo = { id: uniqueId(), title: this.state.todo };
+    let newTodo = { id: uniqueId(), title: this.state.title, body: this.state.body };
     newTodo["done"] = false;
-    newTodo["body"] = "";
     this.props.receiveTodo(newTodo);
-    this.setState( { todo: "" } );
+    this.setState( { title: "", body: "" } );
   }
 
-  updateTodo(e) {
-    this.setState( { todo: e.target.value } );
+  updateTitle(e) {
+    this.setState( { title: e.target.value } );
+  }
+
+  updateBody(e) {
+    this.setState( { body: e.target.value } );
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <input type="text" onChange={this.updateTodo} value={this.state.todo}/>
+        <label>Title</label>
+        <input type="text" onChange={this.updateTitle.bind(this)} value={this.state.title}/>
+        <br/>
+        <label>Body</label>
+        <input type="text" onChange={this.updateBody.bind(this)} value={this.state.body}/>
         <button>Add Todo!</button>
       </form>
     );
