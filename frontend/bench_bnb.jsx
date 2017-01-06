4fa2ea9c8de3564a0dad13788345ffa01cb38b05
Adom Hartell <adomhartell@gmail.com>
2017-01-06 14:54:34 -0800

    add session actions

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as SessionApiUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
+    //TODO Remove below
+    
+    window.login = SessionApiUtil.login;
+    window.signup = SessionApiUtil.signup;
+    window.logout = SessionApiUtil.logout;
+
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
