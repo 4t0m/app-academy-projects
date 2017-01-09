71fda007e4a86ccc17e8b610ff5c7a81406f11ce
Adom Hartell <adomhartell@gmail.com>
2017-01-09 11:44:04 -0800

    add GreetingContainer

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as SessionApiUtil from './util/session_api_util';
 import configureStore from './store/store';
+import Root from './component/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-    //TODO Remove below
     const store = configureStore();
+    const root = document.getElementById('root');
+
+    //TODO Remove below
     window.store = store;
     window.login = SessionApiUtil.login;
     window.signup = SessionApiUtil.signup;
     window.logout = SessionApiUtil.logout;
 
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+    ReactDOM.render(<Root store={ store } />, root);
 });
