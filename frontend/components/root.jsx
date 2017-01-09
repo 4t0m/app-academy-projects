b255df46d1af9bcd57765bdad06e456a5b121192
Adom Hartell <adomhartell@gmail.com>
2017-01-09 11:36:00 -0800

    add Root component

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import App from './app';
 
-const App = ({ children }) => (
-  <div>
-    <h1>BenchBnB</h1>
-    { children }
-  </div>
+const Root = ({ store }) => (
+  <Provider store={ store }>
+    <Router history={ hashHistory }>
+      <Route path="/" component={ App } />
+    </Router>
+  </Provider>
 );
 
-export default App;
+export default Root;
