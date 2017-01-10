783860c752cfd1b9a47a8a77b8437be19a72659f
Adom Hartell <adomhartell@gmail.com>
2017-01-10 14:29:54 -0800

    add router to Root component

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import App from './app';
+
 
 const Root = ({ store }) => (
   <Provider store={ store }>
-    // Router goes here...
+    <Router history={ hashHistory }>
+      <Route path="/" component={ App } />
+    </Router>
   </Provider>
 );
 
