51b059ec757a711fc6a9b9b0c4a6f6edc73b508b
Adom Hartell <adomhartell@gmail.com>
2017-01-10 14:45:13 -0800

    rename greeting to header

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -4,20 +4,19 @@ import { Link } from 'react-router';
 const sessionLinks = () => (
   <nav className="login-signup">
     <Link to="/login" activeClassName="current">Login</Link>
-    &nbsp;or&nbsp;
     <Link to="/signup" activeClassName="current">Sign up!</Link>
   </nav>
 );
 
-const personalGreeting = (currentUser, logout) => (
+const signedInHeader = (currentUser, logout) => (
 	<hgroup className="header-group">
-    <h2 className="header-name">Hi, {currentUser.username}!</h2>
+    <h2 className="header-name">Hi, {currentUser.fname}!</h2>
     <button className="header-button" onClick={logout}>Log Out</button>
 	</hgroup>
 );
 
-const Greeting = ({ currentUser, logout }) => (
-  currentUser ? personalGreeting(currentUser, logout) : sessionLinks()
+const Header = ({ currentUser, logout }) => (
+  currentUser ? signedInHeader(currentUser, logout) : sessionLinks()
 );
 
-export default Greeting;
+export default Header;
