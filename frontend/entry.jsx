83406ddd6fa20d5a4c077bd439ef4dc25eb08104
Adom Hartell <adomhartell@gmail.com>
2017-01-10 14:32:01 -0800

    add Root component to entry file

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as SessionApiUtil from './util/session_api_util';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   //TODO Remove some of this (window asssignments)
@@ -12,5 +13,5 @@ document.addEventListener('DOMContentLoaded', () => {
   window.logout = SessionApiUtil.logout;
 
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to Facebook Clone</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
