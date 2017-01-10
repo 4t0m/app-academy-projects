d51202bdc3c3a3a7027116b8756c9ebdc1888c73
Adom Hartell <adomhartell@gmail.com>
2017-01-10 14:22:53 -0800

    fix problems with users controller and session_api_util

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as SessionApiUtil from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  //TODO Remove some of this (window asssignments)
+  const store = configureStore();
+  window.store = store;
+  window.login = SessionApiUtil.login;
+  window.signup = SessionApiUtil.signup;
+  window.logout = SessionApiUtil.logout;
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Facebook Clone</h1>, root);
 });
--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -1,17 +1,17 @@
 export const signup = (user) => (
   $.ajax({type: "POST",
-          url: "api/user",
+          url: "/api/user",
           data: user})
 );
 
 
 export const login = (user) => (
   $.ajax({type: "POST",
-          url: "api/session",
+          url: "/api/session",
           data: user})
 );
 
 export const logout = () => (
   $.ajax({type: "DELETE",
-          url: "api/session"})
+          url: "/api/session"})
 );
