724e68f8eeb3cc11970271057e5c29f47e8a747c
Adom Hartell <adomhartell@gmail.com>
2017-01-10 15:06:12 -0800

    add header to app

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import HeaderContainer from './header/header_container';
 
 const App = ({ children }) => (
   <div>
     <h1>Facebook Clone</h1>
+    <HeaderContainer/>
     { children }
   </div>
 );
