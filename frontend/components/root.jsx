db83a0eb302170b29dc8ad33ca6a00069b3d94ad
Adom Hartell <adomhartell@gmail.com>
2017-01-10 16:54:29 -0800

    add onEnter callbacks to root component

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -4,12 +4,31 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './app';
 
 
-const Root = ({ store }) => (
-  <Provider store={ store }>
-    <Router history={ hashHistory }>
-      <Route path="/" component={ App } />
-    </Router>
-  </Provider>
-);
+const Root = ({ store }) => {
+
+
+  const _ensureLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (!currentUser) {
+      replace('/login');
+    }
+  };
+
+  const _redirectIfLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (currentUser) {
+      replace('/');
+    }
+  };
+
+  return (
+    <Provider store={ store }>
+      <Router history={ hashHistory }>
+        <Route path="/" component={ App } />
+      </Router>
+    </Provider>
+  );
+
+};
 
 export default Root;
