3695e9c8b549583d41b7fe7b82dcfb51541add57
Adom Hartell <adomhartell@gmail.com>
2017-01-11 16:50:49 -0800

    style signup form

--- frontend/components/welcome/signup_form.jsx
+++ frontend/components/welcome/signup_form.jsx
@@ -18,24 +18,24 @@ class SignupForm extends React.Component {
   }
   render () {
     return (
-      <div className="signup-form col-1-2">
-        <form onSubmit={this.handleSubmit}>
-
+      <div className="signup-and-guest col-1-2">
+        <h1>Sign Up</h1>
+        <form className="signup-form" onSubmit={this.handleSubmit}>
           <section className="name-input">
             <input type="text" onChange={this.update('fname')}
               placeholder="First Name" value={this.state.fname} />
             <input type="text" onChange={this.update('lname')}
               placeholder="Last Name" value={this.state.lname} />
           </section>
-          
+
           <input type="text" onChange={this.update('email')}
             placeholder="Email" value={this.state.email} />
           <input type="password" onChange={this.update('password')}
             placeholder="Password" value={this.state.password} />
 
-          <input type='submit' value="Signup" />
+          <input className="large-button create-account" type='submit' value="Create Account" />
         </form>
-        <button onClick={this.props.guestLogin} value="Guest Login">Guest</button>
+        <button className="large-button guest-login" onClick={this.props.guestLogin} value="Guest Login">Guest Login</button>
       </div>
     );
   }
